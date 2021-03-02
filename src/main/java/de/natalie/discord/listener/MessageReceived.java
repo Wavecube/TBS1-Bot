237212a4a5efde5f35763c83abf3b389451aadc3
WaveCube <BatCube@outlook.de>
2021-03-02 21:52:18 +0100

    Test

--- src/main/java/de/natalie/discord/listener/MessageReceived.java
+++ src/main/java/de/natalie/discord/listener/MessageReceived.java
@@ -1,29 +1,28 @@
 package de.natalie.discord.listener;
 
-import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import org.jetbrains.annotations.NotNull;
 
 public class MessageReceived extends ListenerAdapter {
 
   @Override
-  public void onMessageReceived( @NotNull MessageReceivedEvent e ) {
-
+  public void onPrivateMessageReceived( PrivateMessageReceivedEvent e ) {
+    System.out.println( e.getMessage() );
   }
 
-  @Override
-  public void onPrivateMessageReceived( @NotNull PrivateMessageReceivedEvent e ) {
-    System.out.println( e.getMessage().getContentRaw() );
-    String msg = e.getMessage().getContentRaw();
-    System.out.println("'" + msg.substring( 1 ) + "' : " + msg.startsWith( "!" ));
-    if ( msg.startsWith( "!" ) ) {
-      String command = msg.substring( 1 );
-      switch ( command ) {
-        case "help" -> e.getChannel().sendMessage( e.getAuthor().getName() ).queue();
-        case "command1" -> System.out.println("Test");
-        default -> e.getChannel().sendMessage( command );
-      }
-    }
-  }
+
+  //@Override
+  //public void onPrivateMessageReceived( @NotNull PrivateMessageReceivedEvent e ) {
+  //  System.out.println( e.getMessage().getContentRaw() );
+  //  String msg = e.getMessage().getContentRaw();
+  //  System.out.println("'" + msg.substring( 1 ) + "' : " + msg.startsWith( "!" ));
+  //  if ( msg.startsWith( "!" ) ) {
+  //    String command = msg.substring( 1 );
+  //    switch ( command ) {
+  //      case "help" -> e.getChannel().sendMessage( e.getAuthor().getName() ).queue();
+  //      case "command1" -> System.out.println("Test");
+  //      default -> e.getChannel().sendMessage( command );
+  //    }
+  //  }
+  //}
 }
